8db665a4d4e48371bba134f2b66b6f7716c0153f
robertosixty1 <roberto011herminegildo@gmail.com>
2022-09-10 12:03:34 -0300

    Fix a bug while removing origin shell configuration

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 #!/bin/env python3
 
-from os.path import isfile
+from os.path import exists
 from os import getenv, remove, getcwd, listdir
 from subprocess import run
 
@@ -12,11 +12,8 @@ if getenv("USER") == "root":
     run(["sudo", "-u", USER, "python", __file__])
     exit(0)
 
-if isfile(f"{HOME}/.zshrc"):
-    remove(f"{HOME}/.zshrc")
-
-if isfile(f"{HOME}/.bashrc"):
-    remove(f"{HOME}/.bashrc")
+run(["rm", "-f", f"{HOME}/.zshrc"])
+run(["rm", "-f", f"{HOME}/.bashrc"])
 
 run(["ln", "-s", f"{CWD}/.zshrc", f"{HOME}/.zshrc"])
 run(["ln", "-s", f"{CWD}/.bashrc", f"{HOME}/.bashrc"])

8af9191c7c4fe5ec6f3532a71bb8790de4d76ad1
robertosixty1 <roberto011herminegildo@gmail.com>
2022-09-11 14:47:06 -0300

    Update my ZSH configuration

--- install.py
+++ install.py
@@ -11,12 +11,17 @@ if getenv("USER") == "root":
     run(["sudo", "-u", USER, "python", __file__])
     exit(0)
 
+# Remove previous shell config
 run(["rm", "-f", f"{HOME}/.zshrc"])
 run(["rm", "-f", f"{HOME}/.bashrc"])
 run(["rm", "-f", f"{HOME}/.posixshellrc-personal"])
 
+# Install new shell config
 run(["ln", "-s", f"{CWD}/.zshrc", f"{HOME}/.zshrc"])
 run(["ln", "-s", f"{CWD}/.bashrc", f"{HOME}/.bashrc"])
 run(["ln", "-s", f"{CWD}/.posixshellrc-personal", f"{HOME}/.posixshellrc-personal"])
+
+# Install zsh-syntax-highlighting
+run(["git", "clone", "https://github.com/zsh-users/zsh-syntax-highlighting.git", f"{HOME}/.zsh-syntax-highlighting"])
 
 print("INTALLED SUCCESSFULLY!")
